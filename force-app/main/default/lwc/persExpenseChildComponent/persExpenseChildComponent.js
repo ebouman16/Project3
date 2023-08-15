0710b75b24607b7cd207f4e67532b2441a1055e4
ebouman <elisemeghan@gmail.com>
2023-08-15 11:45:11 -0400

    Personal Expense LWC created, sorted by due date

---
+++ force-app/main/default/lwc/persExpenseChildComponent/persExpenseChildComponent.js
@@ -0,0 +1,34 @@
+import { LightningElement, wire } from 'lwc';
+import getPersExp from '@salesforce/apex/PersExpenseData.getPersExp';
+
+export default class PersExpenseChildComponent extends LightningElement {
+    colConfigs = [
+        {label: "Name", fieldName: "Name", type: "text"},
+        {label: "Type of Expense", fieldName: "Personal_Expense_type__c", type: "text"},
+        {label: "Due Date", fieldName: "Due_Date__c", type: "date", 
+            typeAttriutes:{day:'numeric', month:'short',year:'numeric'}},
+        {label: "Paid?", fieldName: "Paid__c", type: "boolean"} 
+
+    ];
+
+    persExpList = [];
+    error; 
+
+    @wire(getPersExp)
+    wired_persExpList(result) {
+    this.persExpList = [];
+    if (result.data) {
+        this.persExpList = result.data.map(element=>({
+            homeExpId:element.Id,
+            Name:element.Name,
+            Personal_Expense_type__c:element.Personal_Expense_type__c,
+            Due_Date__c:element.Due_Date__c,
+            Paid__c:element.Paid__c
+        }));
+        this.error = undefined;
+    } else if (result.error) {
+        this.persExpList = undefined;
+        this.error = result.error;
+        }
+    }   
+}
